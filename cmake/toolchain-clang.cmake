a54668170bd0d2fe0852529a8c62605fb47f76e6
mincrmatt12 <mincrmatt12@users.noreply.github.com>
2023-09-09 06:21:07 -0400

    Use llvm-objcopy

--- cmake/toolchain-clang.cmake
+++ cmake/toolchain-clang.cmake
@@ -9,11 +9,11 @@ set(CMAKE_ASM_COMPILER ${MWATCH_TOOLCHAIN_DIR}/clang)
 set(CMAKE_LINKER ${MWATCH_TOOLCHAIN_DIR}/clang)
 set(CMAKE_AR ${MWATCH_TOOLCHAIN_DIR}/llvm-ar)
 set(CMAKE_RANLIB ${MWATCH_TOOLCHAIN_DIR}/llvm-ranlib)
-set(OBJCOPY_EXECUTABLE ${MWATCH_TOOLCHAIN_DIR}/llvm-objcopy)
+set(CMAKE_OBJCOPY ${MWATCH_TOOLCHAIN_DIR}/llvm-objcopy)
 
 set(CMAKE_TRY_COMPILE_TARGET_TYPE "STATIC_LIBRARY")
 
 # we always build with debug info because of the crash handler
-set(CMAKE_C_FLAGS_INIT "-nostdlib -mthumb -ggdb3")
-set(CMAKE_CXX_FLAGS_INIT "-nostdlib -mthumb -fno-exceptions -fno-rtti -Wno-register -ggdb3")
+set(CMAKE_C_FLAGS "-nostdlib -mthumb -ggdb3")
+set(CMAKE_CXX_FLAGS "-nostdlib -mthumb -fno-exceptions -fno-rtti -Wno-register -ggdb3")
 set(CMAKE_EXE_LINKER_FLAGS_INIT "-nostdlib -ggdb3")
