59bada42bce39d5534d81da84da1dc5dd1742993
mincrmatt12 <mincrmatt12@users.noreply.github.com>
2023-09-10 00:15:02 -0400

    Force local-exec tls model
    the other models assume dynamic linking which we do not perform, thus
    breaking in a myriad of interesting ways

--- bt/cmake/toolchain-clang-bt.cmake
+++ bt/cmake/toolchain-clang-bt.cmake
@@ -1,7 +1,7 @@
 include(${CMAKE_CURRENT_LIST_DIR}/toolchain-clang.cmake)
 
 set(MWATCH_TOOLCHAIN_SYSROOT ${MWATCH_TOOLCHAIN_DIR}/../lib/clang-runtimes/arm-none-eabi/armv7em_hard_fpv4_sp_d16)
-set(MWATCH_TOOLCHAIN_COMMON_FLAGS "--target=arm-none-eabif -mfloat-abi=hard -march=armv7em -mcpu=cortex-m4 -mfpu=fpv4-sp-d16 --sysroot ${MWATCH_TOOLCHAIN_SYSROOT}")
+set(MWATCH_TOOLCHAIN_COMMON_FLAGS "--target=arm-none-eabif -ftls-model=local-exec -mfloat-abi=hard -march=armv7em -mcpu=cortex-m4 -mfpu=fpv4-sp-d16 --sysroot ${MWATCH_TOOLCHAIN_SYSROOT}")
 
 set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${MWATCH_TOOLCHAIN_COMMON_FLAGS}")
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${MWATCH_TOOLCHAIN_COMMON_FLAGS}")
--- os/cmake/toolchain-clang-rp.cmake
+++ os/cmake/toolchain-clang-rp.cmake
@@ -1,7 +1,7 @@
 include(${CMAKE_CURRENT_LIST_DIR}/toolchain-clang.cmake)
 
 set(MWATCH_TOOLCHAIN_SYSROOT ${MWATCH_TOOLCHAIN_DIR}/../lib/clang-runtimes/arm-none-eabi/armv6m_soft_nofp)
-set(MWATCH_TOOLCHAIN_COMMON_FLAGS "--target=armv6m-none-eabi -mfloat-abi=soft -march=armv6m -mcpu=cortex-m0plus --sysroot ${MWATCH_TOOLCHAIN_SYSROOT}")
+set(MWATCH_TOOLCHAIN_COMMON_FLAGS "--target=armv6m-none-eabi -ftls-model=local-exec -mfloat-abi=soft -march=armv6m -mcpu=cortex-m0plus --sysroot ${MWATCH_TOOLCHAIN_SYSROOT}")
 
 set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${MWATCH_TOOLCHAIN_COMMON_FLAGS}")
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${MWATCH_TOOLCHAIN_COMMON_FLAGS}")
