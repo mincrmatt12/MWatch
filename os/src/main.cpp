5f22a43a66973d0a0a36e6bd35571b604e124d51
mincrmatt12 <mincrmatt12@users.noreply.github.com>
2023-09-10 21:03:12 -0400

    Test pattern on display
    wow the display works it's incredible

--- os/src/main.cpp
+++ os/src/main.cpp
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <pico/platform.h>
+#include "hw/pwr.h"
 #include "hw/screen.h"
 #include "kcore/delay.h"
 
@@ -7,6 +8,29 @@
 
 // Global task manager
 mwk::exc::task_manager global_tm;
+
+void draw_frame(int idx) {
+	for (int color = 0; color < 64 ;++color) {
+		for (int y = 130; y < 150; ++y) mwos::screen::set_pixel(80+color, y, (idx+color) % 0x40);
+	}
+}
+
+void set_big_text(int x, int y, uint8_t c) {
+	x -= 100;
+	y -= 119;
+
+	x*= 2;
+	y*= 2;
+
+	x += 100;
+	y += 119;
+
+	for (int ox = 0; ox < 2; ++ox) {
+		for (int oy = 0; oy < 2; ++oy) {
+			mwos::screen::set_pixel(x+ox, y+oy, c);
+		}
+	}
+}
 
 mwk::task<void> main_task() {
 	puts("hi!");
@@ -22,38 +46,56 @@ mwk::task<void> main_task() {
 
 	// write "HI" and a line
 	
-	set_pixel(100, 120, 0x3f);
-	set_pixel(100, 119, 0x3f);
-	set_pixel(100, 121, 0x3f);
-	set_pixel(101, 120, 0x3f);
-	set_pixel(102, 119, 0x3f);
-	set_pixel(102, 120, 0x3f);
-	set_pixel(102, 121, 0x3f);
-
-	set_pixel(104, 121, 0x3f);
-	set_pixel(104, 119, 0x3f);
-	set_pixel(105, 119, 0x3f);
-	set_pixel(105, 120, 0x3f);
-	set_pixel(105, 121, 0x3f);
-	set_pixel(106, 121, 0x3f);
-	set_pixel(106, 119, 0x3f);
-
-	for (int color = 0; color < 64 ;++color) {
-		set_pixel(80+color, 130, color);
+	for (int y = 0; y < 50; ++y) {
+		for (int x = 120 - y; x < 120 + y; ++x) {
+			set_pixel(x, y, (x + y) % 0x40);
+		}
 	}
+	for (int y = 240-50; y < 240; ++y) {
+		int off = 240 - y;
+		for (int x = 120 - off; x < 120 + off; ++x) {
+			set_pixel(x, y, (x + y) % 0x40);
+		}
+	}
+	
+	set_big_text(100, 120, 0b100100);
+	set_big_text(100, 119, 0b100100);
+	set_big_text(100, 121, 0b100100);
+	set_big_text(101, 120, 0b100100);
+	set_big_text(102, 119, 0b100100);
+	set_big_text(102, 120, 0b100100);
+	set_big_text(102, 121, 0b100100);
+
+	set_big_text(104, 121, 0b010010);
+	set_big_text(104, 119, 0b010010);
+	set_big_text(105, 119, 0b010010);
+	set_big_text(105, 120, 0b010010);
+	set_big_text(105, 121, 0b010010);
+	set_big_text(106, 121, 0b010010);
+	set_big_text(106, 119, 0b010010);
+
+	set_big_text(108, 119, 0b111001);
+	set_big_text(108, 120, 0b111001);
+	set_big_text(108, 121, 0b111001);
+	set_pixel(116, 122, 0);
+	set_pixel(117, 122, 0);
+	set_pixel(116, 123, 0);
+	set_pixel(117, 123, 0);
 
 	puts("ready");
 
 	// the multicolored line
 	
+	int idx = 0;
 	while (true) {
 		co_await scanout_and_wait_frame();
-		puts("frame");
-		co_await mwos::delay.by(16);
+		draw_frame(idx++);
 	}
 }
 
 int main() {
+	mwos::pwr::init();
+
 	// call prepare_hw
 	global_tm.detach_task(main_task()); // todo: check result
 
